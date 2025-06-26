e4712b1d653987a4e6a1cc8e166d113c708c9c42
Semi56 <swiety56@gmail.com>
2025-06-26 10:52:22 +0200

    fixed formatting in main.py, user_prompt is now being sstored in a list

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
-
-client = genai.Client(api_key=api_key)
-
-#command line argument for prompt
 import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
-if len(sys.argv) < 2:
-    print("Please provide an argument!")
-    sys.exit(1)
+def main():
+    load_dotenv()
 
-argument = sys.argv[1]
+    #command line argument for prompt
+    if len(sys.argv) < 2:
+        print("Please provide an argument!")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=argument
-)
-print(response.text)
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+   
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    generate_content(client, messages)
+    
 
-prompt_tokens = response.usage_metadata.prompt_token_count
-response_tokens = response.usage_metadata.candidates_token_count
+def generate_content(client, messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print(response.text)
 
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
+
+if __name__ == "__main__":
+    main()
