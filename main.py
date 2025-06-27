e8cd8810dd6b40f4f078bd25502b01f511bb1288
Semi56 <swiety56@gmail.com>
2025-06-27 09:55:01 +0200

    main.py now accepts --verbose argument

--- main.py
+++ main.py
@@ -7,33 +7,45 @@ from google.genai import types
 def main():
     load_dotenv()
 
-    #command line argument for prompt
-    if len(sys.argv) < 2:
-        print("Please provide an argument!")
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+    if not args:
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "What is the color of the sky?"')
         sys.exit(1)
-    user_prompt = sys.argv[1]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-   
+
+    user_prompt = " ".join(args)
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")   
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
-    generate_content(client, messages)
+              
+    generate_content(client, messages, verbose)
+    
     
 
-def generate_content(client, messages):
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    
+    print("Response:")    
     print(response.text)
-
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()
